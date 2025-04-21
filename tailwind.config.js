338ea0bdeb1642a646c0a2d1fd9b4ce136c2f955
Arlis-Coder <lana43067@gmail.com>
2025-04-21 20:01:15 +0700

    Push template

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
